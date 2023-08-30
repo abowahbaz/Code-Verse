1a5f7fd9b7fa4b4346445ac8d218717df7ddc1fc
Ahmed Mohammed Wahba <67748256+abowahbaz@users.noreply.github.com>
2023-08-30 08:09:58 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,114 +1,89 @@
-const hamburger = document.querySelector(".hamburger");
+const resp_menu = document.querySelector(".resp-menu");
 const navMenu = document.querySelector(".nav-menu");
 
+resp_menu.addEventListener("click", () => {
+  resp_menu.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-
-hamburger.addEventListener("click",() => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-
-})
-
-document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click",()=>{
-    hamburger.classList.remove("active");
+document.querySelectorAll(".nav-item").forEach((n) =>
+  n.addEventListener("click", () => {
+    resp_menu.classList.remove("active");
     navMenu.classList.remove("active");
-}))
+  })
+);
 
+const loader = document.querySelector(".loader");
 
+window.addEventListener("load", () => {
+  loader.classList.add("loader--hidden");
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild(loader);
+  });
+});
 
-const y = document.querySelector('#year');
-y.innerHTML=new Date().getFullYear();
+const y = document.querySelector("#year");
+y.innerHTML = new Date().getFullYear();
 
-
-
-function ValidateEmail(x) 
-{
-
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(x.value.match(mailformat))
-{
-return true;
-}
-else
-{
-
-
-return false;
-}
-
- 
+function ValidateEmail(x) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (x.value.match(mailformat)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function checkMsg(msg)
-{
-  if(msg.value=="" || msg.value==" ") return false;
-
+function checkMsg(msg) {
+  if (msg.value == "" || msg.value == " ") return false;
   else return true;
-  
 }
 
+var modal = document.getElementById("myModal");
+var inv = document.getElementById("inv");
+var btn = document.querySelector(".send-btn");
+var inv_msg = document.querySelector(".invModal .inv-modal-content p");
+var msg = document.getElementById("iMsg");
+var span = document.getElementsByClassName("close")[0];
 
-  
+var email = document.querySelector("#iMail");
 
-
-
-    var modal = document.getElementById("myModal");
-    var inv = document.getElementById("inv");
-    var btn = document.querySelector(".send-btn");
-    var inv_msg = document.querySelector(".invModal .inv-modal-content p");
-    var msg = document.getElementById("iMsg");
-    var span = document.getElementsByClassName("close")[0];
-
-
-    var email = document.querySelector("#iMail");
-  
-  btn.onclick = function() {
-    
-    if(ValidateEmail(email) && checkMsg(msg))
-    {
+btn.onclick = function () {
+  if (ValidateEmail(email) && checkMsg(msg)) {
     modal.style.display = "block";
-    document.querySelectorAll("input").forEach(n => n.value = "");
+    document.querySelectorAll("input").forEach((n) => (n.value = ""));
 
-
-    
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-  
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
       modal.style.display = "none";
-    }
-  }
-        
-}
+    };
 
-else
-{
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+  } else {
     inv.style.display = "block";
 
-    if(checkMsg(msg)==false &&  ValidateEmail(email))inv_msg.innerHTML = "Your Message Is Empty"; 
-    else if(checkMsg(msg)==false && ValidateEmail(email)==false)
-    inv_msg.innerHTML = "Invalid Data";
+    if (checkMsg(msg) == false && ValidateEmail(email))
+      inv_msg.innerHTML = "Your Message Is Empty";
+    else if (checkMsg(msg) == false && ValidateEmail(email) == false)
+      inv_msg.innerHTML = "Invalid Data";
+    else if (checkMsg(msg) == true && ValidateEmail(email) == 0)
+      inv_msg.innerHTML = "Invalid Email";
 
-    else if(checkMsg(msg)==true && ValidateEmail(email)==0)
-    inv_msg.innerHTML = "Invalid Email";
-    
-  // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-    modal.style.display = "none";
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == inv) {
+        inv.style.display = "none";
+      }
+    };
   }
-  
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == inv) {
-      inv.style.display = "none";
-    }
-  }
-}
-
-
-
-}
+};
---
+++ readme.md
@@ -0,0 +1,17 @@
+# ❓ About The Repo:
+
+CodeVerse Is A Front-End Project That I Made To Test My Skils In
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=plastic&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=plastic&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=plastic&logo=javascript&logoColor=%23F7DF1E) .
+
+# What is CodeVerse 💻:
+
+CodeVerse Is A Learning Platform That Provides Several Study Plans
+Explained In Detail To Help You Enhance Your Skills, Expand Your
+Knowledge and Pass Your Coding Interview.
+
+# Inspiration 💡:
+
+CodeVerse as a project is meant to be a warehouse of all awesome free courses about technology
+Color palette is inspired from LeetCode and CodeChef.
